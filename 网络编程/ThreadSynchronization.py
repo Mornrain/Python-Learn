cea1fc1220a2f25d00c2b4d396c3691a29f41442
Mornrain <112masata@gmail.com>
2019-09-21 20:07:38 +0800

    线程同步开发实践

---
+++ 网络编程/ThreadSynchronization.py
@@ -0,0 +1,63 @@
+#!/user/bin/python3
+
+# 线程同步问题
+
+import threading
+import time
+
+class myThread(threading.Thread):
+
+    def __init__(self,threadID,name,delay):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.delay = delay
+
+    def run(self):
+        print("开启线程: "+ self.name)
+        # 获取锁，用于线程同步
+        threadLock.acquire()
+        print_time(self.name,self.delay,5)
+        # 释放锁，开启下一个线程
+        threadLock.release()
+
+def print_time(threadName,threadDelay,counter):
+    while counter:
+        time.sleep(threadDelay)
+        print("%s:  %s " %(threadName,time.ctime(time.time())))
+        counter -= 1
+# 创建锁对象
+threadLock=threading.Lock()
+
+# 创建线程列表
+threads=[]
+
+# 创建新线程
+thread1 = myThread(1,"Thread-1: ",3)
+thread2 = myThread(2,"Thread-2: ",7)
+
+# 开启新线程
+thread1.start()
+thread2.start()
+
+# 添加线程到线程列表
+threads.append(thread1)
+threads.append(thread2)
+
+# 等待所有线程执行完毕
+for i in threads:
+    i.join()
+print("退出主线程")
+
+
+
+
+
+
+
+
+
+
+
+
+
